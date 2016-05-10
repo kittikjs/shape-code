e3fb8b9aca7d44578021af036e269cc33b92780a
Eugene Obrezkov <ghaiklor@gmail.com>
2016-05-10 23:07:00 +0300

    docs(shape): Update example

--- examples/simple.js
+++ examples/simple.js
@@ -4,7 +4,7 @@ const Code = require('../lib/Code');
 const cursor = require('kittik-cursor').create().reset();
 
 Code.create(cursor, {
-  code: `const sentence = 'KittikJS: shape for code rendering'\nconsole.log(sentence);`,
+  code: `for (let i = 0; i < 1234; i++) {console.log('KittikJS: shape for code rendering');}`,
   x: 'center',
   y: 'middle'
 }).render();
